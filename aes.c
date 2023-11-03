a71ca09baeb91dd5b71ee3ed15dfb07986b614b3
S-onny <83006264+S-onny@users.noreply.github.com>
2023-11-03 12:53:31 +0900

    Create Repository

---
+++ aes.c
@@ -0,0 +1,52 @@
+#include "aes.h"
+
+// «–π¯_¿Ã∏ß
+char SUBMISSION_INFO[256] = "2023123456_»´±Êµø";
+
+// ∞¸∑√ µ•¿Ã≈Õ ≈∏¿‘ ¡§¿« ≥ªø™
+// 128-bit block
+// typedef uint8_t AES_STATE_t[16];
+// 128-bit masterkey
+// typedef uint8_t AES128_KEY_t[16];
+
+void AES128_enc(AES_STATE_t C, AES_STATE_t P, AES128_KEY_t K128)
+{
+    //...
+}
+
+void AES128_dec(AES_STATE_t P, AES_STATE_t C, AES128_KEY_t K128)
+{
+    //...
+}
+
+// ∞¸∑√ µ•¿Ã≈Õ ≈∏¿‘ ¡§¿« ≥ªø™
+// 128-bit block
+// typedef uint8_t AES_STATE_t[16];
+// 192-bit masterkey
+// typedef uint8_t AES192_KEY_t[24];
+
+void AES192_enc(AES_STATE_t C, AES_STATE_t P, AES192_KEY_t K192)
+{
+    //...
+}
+
+void AES192_dec(AES_STATE_t P, AES_STATE_t C, AES192_KEY_t K192)
+{
+    //...
+}
+
+// ∞¸∑√ µ•¿Ã≈Õ ≈∏¿‘ ¡§¿« ≥ªø™
+// 128-bit block
+// typedef uint8_t AES_STATE_t[16];
+// 256-bit masterkey
+// typedef uint8_t AES256_KEY_t[32];
+
+void AES256_enc(AES_STATE_t C, AES_STATE_t P, AES256_KEY_t K256)
+{
+    //...
+}
+
+void AES256_dec(AES_STATE_t P, AES_STATE_t C, AES256_KEY_t K256)
+{
+    //...
+}
